549db859b9d45b1a6c93be0701ee430953c6e2b2
fuke-daily <fuke@daily.com>
2026-07-19 10:41:52 +0800

    temp: 还原版本号，等GitHub网络恢复后重新发布

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fuke.daily"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.4.1"
+        versionCode = 7
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
